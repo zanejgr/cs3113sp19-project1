f6579bd45732192a6430a6ae15a61e3c71675cf5
Zane Gray <zanejgr@gmail.com>
2019-03-15 04:46:00 +0000

    pipe still not working

--- oush.c
+++ oush.c
@@ -1,15 +1,17 @@
 #include "proj1.h"
 #define READ_END 0
 #define WRITE_END 1
-
+//global variables
+int wstatus = -1;
+unsigned short cmdflags;	//set if  PIPE is specified
+long pos;			//for going back after counting the number of pipes
 //function prototypes
 int BGExec(char**execvbuf);
+int pipeExec(int infile, int outfile, char*cmd);
 
 int main(int argc,char**argv){		
 	char  inbuf[LINE_MAX];      	//hold the current command
 	char* execvbuf[LINE_MAX];	//current command after processing
-	int wstatus = -1;
-	unsigned short cmdflags;	//set if BG or PIPE is specified
 
 	//open the batchfile
 	if(argc!=2){
@@ -21,13 +23,17 @@ int main(int argc,char**argv){
 		log_err("Invalid argument %s",argv[1]);
 		return 1;
 	}		
+	//read the file, executing each line as per spec
 	while(fgets(inbuf,LINE_MAX, fp)!=NULL){
-		int pipefd[2][2];	//2 pipes, one for previous and one for next process
+		fflush(stdin);
+		fflush(stdout);
+		fflush(stderr);
 		int wstatus;
-		pid_t childpid;
+		pid_t pid;
 
 		//null-terminate
 		inbuf[strlen(inbuf)-1]='\0';
+		int numPipes=0;
 		int execvbufindex=0;
 		execvbuf[execvbufindex++]=strtok(inbuf," ");
 
@@ -41,8 +47,62 @@ int main(int argc,char**argv){
 			continue;
 		}
 
+		//if pipe is specified
+		if(execvbufindex>2&&!strncmp(execvbuf[execvbufindex-2],"PIPE",4)){
+			execvbuf[execvbufindex-2]='\0';
+			if(!cmdflags)	pos = ftell(fp);
+			cmdflags = 1;
+			numPipes++;
+
+			continue;
+		}
+
+		//if pipe was specified in a previous command
+		if(cmdflags){
+			long tmp = ftell(fp);
+			fseek(fp,pos,SEEK_SET);
+			pos = tmp;
+			int pipes[numPipes][2];
+			for(int i = 0; i < numPipes; i++)
+				//create pipes for each command in the chain
+				if(!pipe(pipes[i])){
+					log_err("unable to pipe on the %dth pipe",i);
+					return -1;
+				}
+		//allow the first and last commands in the chain to read 
+			//pass each command along to a helper subroutine, including the last in the chain 
+			if(!fgets(inbuf,LINE_MAX,fp)){
+				log_err("unable to re-get the first command in a chain");
+				return -1;
+			}
+			if(!pipeExec(0,pipes[0][READ_END],inbuf)){
+				log_err("unable to exec %s as the first cmd in a chain",inbuf);
+				return -1;
+			}
+			for(int i = 1; i < numPipes-1;i++){
+				if(!fgets(inbuf,LINE_MAX,fp)){
+					log_err("unable to re-get the %dth commandin a chain",i);
+					return -1;
+				}
+				if(!pipeExec(pipes[i][READ_END],pipes[i][WRITE_END],inbuf)){
+					log_err("unable to exec %s as the %dth cmd in a chain",inbuf,i);
+					return -1;
+				}
+			}
+			if(!fgets(inbuf,LINE_MAX,fp)){
+				log_err("unable to re-get the first command in a pipe chain");
+				return -1;
+			}
+			if(!pipeExec(pipes[numPipes-1][WRITE_END],1,inbuf)){
+				log_err("couldn't exec %s as last cmd in a pipe chain",inbuf);
+				return -1;
+			}
+			cmdflags = 0;
+			numPipes = 0;
+			continue;
+		}
 		//no option specified
-		switch(childpid=fork()){
+		switch(pid=fork()){
 			case -1:log_err("fork");
 				return 1;
 			case 0:	
@@ -50,16 +110,17 @@ int main(int argc,char**argv){
 				//execution should never reach this point
 				log_err("exec with arguments %s", inbuf);
 				return 1;
-			default:waitpid(childpid,&wstatus,0);
-
+			default:waitpid(pid,&wstatus,0);
 				if(wstatus){
 					log_err("child returned unsuccessfully with value");
 					return 1;
 				}
 				break;
 		}
+		fflush(stdin);
 	}return 0;
 }
+//runs a process in the background
 int BGExec(char**execvbuf){
 	if(!freopen("/dev/null","w",stdout))
 		log_err("BGExec freopen");
@@ -69,4 +130,36 @@ int BGExec(char**execvbuf){
 	log_err("bgexec");
 	return 1;
 }
-
+//runs a process with input and output redirected to the specified filedescriptor
+int pipeExec(int infile, int outfile, char*cmd){
+	int pid = -1;
+	int wstatus;
+	int execvbufindex = 0;
+	char* execvbuf[LINE_MAX];
+	//current command after processing
+	execvbuf[execvbufindex++]=strtok(cmd," ");
+	while(execvbuf[execvbufindex++]=strtok(NULL," "));
+	if(execvbufindex>2&&!strncmp(execvbuf[execvbufindex-2],"PIPE",4))
+		execvbuf[execvbufindex-2]='\0';
+	//fork-dup2-exec
+	switch(pid=fork()){
+		case -1:log_err("fork in pipe");
+			return 1;
+		case 0://child
+			if((!dup2(infile,0))||(!dup2(outfile,1)))
+				log_err("dup2 %i,%i",infile,outfile);
+			if(!execvp(execvbuf[0],execvbuf))
+				log_err("pipeExec exec %s",cmd);
+			//execution should never reach this point
+			log_err("pipeExec %s",cmd);
+			return 1;
+		default:
+			waitpid(pid,&wstatus,0);
+			close(infile);
+			close(outfile);
+	}
+	fflush(stdin);
+	fflush(stdout);
+	fflush(stderr);
+	return 0;
+}
